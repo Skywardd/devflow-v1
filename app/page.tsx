3ad7b10514520bfa5de5a53e041e795571b39150
Skywardd <yvette.danailova@gmail.com>
2025-08-13 13:40:40 +0300

    Setup tailwind themes,base,components & utilities

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 const Home = () => {
-  return <h1 className="text-3xl bg-amber-50">devflow</h1>;
+  return (
+    <div className="background-light700_dark300 ">
+      <h1 className="text-3xl text-light-500 mb-4">devflow</h1>
+      <div className="bg-light-500 text-white p-4 rounded mb-4">
+        Background with light-500 color
+      </div>
+      <p className="text-light-400">This should be light-400 color</p>
+    </div>
+  );
 };
 
 export default Home;
