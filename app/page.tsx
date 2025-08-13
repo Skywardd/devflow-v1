ac0857deeb998de68984bba8a414b7bd7cacc7f8
Skywardd <yvette.danailova@gmail.com>
2025-08-13 14:34:07 +0300

    Assets, metadata & fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 const Home = () => {
   return (
-    <div className="background-light700_dark300 ">
+    <div className="">
       <h1 className="text-3xl text-light-500 mb-4">devflow</h1>
       <div className="bg-light-500 text-white p-4 rounded mb-4">
-        Background with light-500 color
+        <h1 className="font-bold font-space-grotesk">
+          {" "}
+          Background with light-500 color
+        </h1>
       </div>
       <p className="text-light-400">This should be light-400 color</p>
     </div>
